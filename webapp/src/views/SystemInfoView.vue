3ec2d8c193e031c5ca0547203be7c6797c86c663
soylentOrange <robert.wendlandt@uksh.de>
2024-11-09 19:20:25 +0100

    update config profiles

--- webapp/src/views/SystemInfoView.vue
+++ webapp/src/views/SystemInfoView.vue
@@ -83,7 +83,7 @@ export default defineComponent({
             }
 
             const fetchUrl =
-                'https://api.github.com/repos/tbnobody/OpenDTU/compare/' + this.systemDataList.git_hash + '...HEAD';
+                'https://api.github.com/repos/soylentOrange/OpenDTU/compare/' + this.systemDataList.git_hash + '...HEAD';
 
             fetch(fetchUrl)
                 .then((response) => {
